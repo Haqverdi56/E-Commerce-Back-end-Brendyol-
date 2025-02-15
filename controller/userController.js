1d671b4091f59865bc9f48416efe5cbf6bed5e91
Haqverdi56 <qhaqverdi56@gmail.com>
2025-02-15 06:34:31 +0400

    fix nodemailer

--- controller/userController.js
+++ controller/userController.js
@@ -1,108 +1,118 @@
-const User = require("../models/UserModel");
-const nodemailer = require("nodemailer");
-const userModel = require("../models/UserModel");
-var jwt = require("jsonwebtoken");
-let privateKey = "ironmaidenironmaidenironmaidenironmaiden";
+const User = require('../models/UserModel');
+const nodemailer = require('nodemailer');
+const userModel = require('../models/UserModel');
+var jwt = require('jsonwebtoken');
+let privateKey = 'ironmaidenironmaidenironmaidenironmaiden';
 
 const transporter = nodemailer.createTransport({
-  direct: true,
-  host: "smtp.gmail.com",
-  port: 465,
-  auth: {
-    user: "ecommercedeveloper1@gmail.com",
-    pass: "uzvxyehggdomgaxm",
-  }, 
-  secure: true,
+	service: 'gmail',
+	direct: true,
+	host: 'smtp.gmail.com',
+	port: 465,
+	auth: {
+		user: 'gamerhaki56@gmail.com',
+		pass: 'inmb dtlh meks xjaw',
+	},
+	secure: true,
 });
 
 const userController = {
-  getUsers: async (req, res) => {
-    try {
-      const users = await User.find().exec();
-      res.json(users);
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-  },
-  register: async (req, res) => {
-    const { email, password, userName } = req.body;
-    console.log(req.body);
-    try {
-      const existingUser = await userModel.findOne({ email });
+	getUsers: async (req, res) => {
+		try {
+			const users = await User.find().exec();
+			res.json(users);
+		} catch (err) {
+			res.status(500).json({ message: err.message });
+		}
+	},
+	register: async (req, res) => {
+		const { email, password, userName } = req.body;
+		console.log(req.body);
+		try {
+			const existingUser = await userModel.findOne({ email });
 
-      if (existingUser) {
-        return res.status(409).json({ message: 'Email already exists' });
-      }
+			if (existingUser) {
+				return res.status(409).json({ message: 'Email already exists' });
+			}
 
-      const newUser = await userModel.create({ email, password, userName });
-      return res.status(201).json(newUser);
-    } catch (err) {
-      console.error(err);
-      return res.status(500).json({ error: 'Server error' });
-    }
-  },
-  login: async (req, res) => {
-    let {email, password, userName} = req.body;
-    const user = new userModel( {
-      email: email,
-      password: password,
-      favorites: [],
-      confirmCode: '',
-      isVerified: false
-    })
+			const newUser = await userModel.create({ email, password, userName });
+			return res.status(201).json(newUser);
+		} catch (err) {
+			console.error(err);
+			return res.status(500).json({ error: 'Server error' });
+		}
+	},
+	login: async (req, res) => {
+		let { email, password, userName } = req.body;
+		const user = new userModel({
+			email: email,
+			password: password,
+			favorites: [],
+			confirmCode: '',
+			isVerified: false,
+		});
 
-    userModel.findOne({ email: email, password: password })
-      .then (doc => {
-        if (doc) {
+		userModel
+			.findOne({ email: email, password: password })
+			.then((doc) => {
+				if (doc) {
+					let confirmCode = Math.floor(Math.random() * 999999);
+					doc.confirmCode = confirmCode;
 
-          let confirmCode = Math.floor(Math.random() * 999999);
-          doc.confirmCode = confirmCode;
+					doc
+						.save()
+						.then((saveDoc) => {
+							res.json(saveDoc);
+						})
+						.catch((saveErr) => {
+							res.status(500).json(saveErr);
+						});
 
-          doc.save()
-          .then(saveDoc => {
-            res.json(saveDoc)
-          }) .catch(saveErr => {
-            res.status(500).json(saveErr)
-          })
-          
-          var mailOptions = {
-            from: "ecommercedeveloper1@gmail.com",
-            to: doc.email,
-            subject: "Login Confirm Code",
-            text: "Confirm Code: " + confirmCode,
-          };
+					var mailOptions = {
+						from: 'gamerhaki56@gmail.com',
+						to: doc.email,
+						subject: 'Login Confirm Code',
+						text: 'Confirm Code: ' + confirmCode,
+					};
 
-          transporter.sendMail(mailOptions, function (error, info) {
-            if (error) {
-              return console.log("error 1", error);
-            }
-          });
-        } else {
-          res.status(404).json({ message: "Not found" });
-        }
-      }) 
-      .catch ((err) => {
-        res.status(500).json(err);
-      })
-  },
-  confirmCode: (req, res) => {
-    let confirmCode = req.body.confirmCode;
-    
-    userModel.findOne({ confirmCode: confirmCode })
-    .then(doc => {
-      if (doc) {
-        let token = jwt.sign({ email: userModel.email }, privateKey, {
-          algorithm: "HS256",
-          expiresIn: "5h",
-        });
-        res.json({ token: token, user: doc });
-      }
-    }) .catch(err => {
-      res.status(500).json(err);
-    })
-  }
+					transporter.sendMail(mailOptions, function (error, info) {
+						if (error) {
+							console.error(
+								'Mail gönderim hatası:',
+								error.response || error.message
+							);
+						} else {
+							console.log('Mail gönderildi:', info.response);
+						}
+					});
+				} else {
+					res.status(404).json({ message: 'Not found' });
+				}
+			})
+			.catch((err) => {
+				res.status(500).json(err);
+			});
+	},
+	confirmCode: (req, res) => {
+		let confirmCode = req.body.confirmCode;
+
+		userModel
+			.findOne({ confirmCode: confirmCode })
+			.then((doc) => {
+				if (doc) {
+					let token = jwt.sign({ email: userModel.email }, privateKey, {
+						algorithm: 'HS256',
+						expiresIn: '5h',
+					});
+					res.json({ token: token, user: doc });
+				}
+			})
+			.catch((err) => {
+				res.status(500).json(err);
+			});
+	},
 };
 
 module.exports = {
-  userController,
+	userController,
 };
